eed0815a9493f949dd96d0ca5dec2a863d70ceb6
Andrew Peng <andrewpeng38@gmail.com>
2025-03-12 16:28:59 -0400

    Updated to simpler template

--- android/app/src/main/kotlin/com/example/verygoodcore/fitpet/adventure/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.verygoodcore.fitpet_adventure
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- lib/main_production.dart
+++
@@ -1,6 +0,0 @@
-import 'package:fitpet_adventure/app/app.dart';
-import 'package:fitpet_adventure/bootstrap.dart';
-
-void main() {
-  bootstrap(() => const App());
-}
